d7669d43789a01c498511e0f8471eef350745d3e
lucidrains <lucidrains@gmail.com>
2024-06-04 15:55:07 -0700

    0.1.8

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 setup(
   name = 'st-moe-pytorch',
   packages = find_packages(exclude=[]),
-  version = '0.1.7',
+  version = '0.1.8',
   license='MIT',
   description = 'ST - Mixture of Experts - Pytorch',
   author = 'Phil Wang',
